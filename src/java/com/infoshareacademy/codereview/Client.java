85f456ea7dc8036ddc2cc8e61821a4eede5b5190
Rafał Zakrzewski <Rafal.Zakrzewski@staples-solutions.com>
2019-08-10 13:44:58 +0200

    First change

--- src/java/com/infoshareacademy/codereview/Client.java
+++ src/java/com/infoshareacademy/codereview/Client.java
@@ -1,31 +1,31 @@
 package java.com.infoshareacademy.codereview;
 
 public class Client {
-//    private String imie;
-//    private String surname;
-//    private Long age;
-//
-//    public String getName() {
-//        return imie;
-//    }
-//
-//    public void setName(String name) {
-//        this.imie = name;
-//    }
-//
-//    public String getSurname() {
-//        return surname;
-//    }
-//
-//    public void setSurname(String surname) {
-//        this.surname = surname;
-//    }
-//
-//    public Long getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(Long age) {
-//        this.age = age;
-//    }
+    private String imie;
+    private String surname;
+    private Long age;
+
+    public String getName() {
+        return imie;
+    }
+
+    public void setName(String name) {
+        this.imie = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
 }
